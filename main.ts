884a71ae2efddf6a1ab10ac438db6e875d70aa20
tt02007 <91233572+tt02007@users.noreply.github.com>
2023-03-24 08:39:02 +0800

    ok

--- main.ts
+++ main.ts
@@ -1,33 +1,35 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showIcon(IconNames.Heart)
-    basic.showNumber(receivedNumber)
-    group = receivedNumber
+input.onButtonPressed(Button.A, function () {
     group += 1
-    radio.setGroup(group)
-    basic.pause(1000)
-    radio.sendNumber(group)
-    basic.pause(1000)
-    radio.sendString(密碼)
 })
 input.onButtonPressed(Button.AB, function () {
-    密碼 = "abc"
-    group += 1
+    if (group != 99) {
+        group += 1
+        basic.showNumber(group)
+        basic.showString(密碼)
+        radio.setGroup(group)
+        radio.sendValue(密碼, group)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
+input.onButtonPressed(Button.B, function () {
     radio.setGroup(group)
     basic.showNumber(group)
-    basic.showString(密碼)
-    radio.sendNumber(group)
-    radio.sendString(密碼)
+    if (group == 1) {
+        密碼 = "xyz"
+    }
+    basic.showIcon(IconNames.Yes)
 })
-radio.onReceivedString(function (receivedString) {
+radio.onReceivedValue(function (name, value) {
+    basic.showIcon(IconNames.Heart)
+    basic.showNumber(value)
+    basic.showString(name)
+    密碼 = name
+    group = value
     basic.showIcon(IconNames.Happy)
-    basic.showString(receivedString)
-    密碼 = receivedString
 })
 let 密碼 = ""
 let group = 0
-radio.setGroup(4)
-group = 99
-basic.showNumber(4)
-basic.forever(function () {
-	
-})
+radio.setGroup(99)
+group = 0
+密碼 = "abc"
